483d110b206ac5d4d9c7adb023e330661590fda4
anedomansky <anedomansky@googlemail.com>
2021-10-09 10:59:38 +0200

    Added unit tests.

--- src/accordion-item/AccordionItem.test.tsx
+++ src/accordion-item/AccordionItem.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import AccordionItem from './AccordionItem';
+import AccordionSummary from '../accordion-summary/AccordionSummary';
 
 test('Renders the AccordionItem component', () => {
     const { container } = render(
@@ -20,4 +21,21 @@ test('Renders the AccordionItem component with custom class', () => {
     expect(container).toBeInTheDocument();
     expect(getByTestId('item').classList.contains('test-item')).toBeTruthy();
     expect(getByTestId('item').classList.length).toBe(1);
+});
+
+test('Renders the AccordionItem component', () => {
+    const { container, getByTestId } = render(
+        <AccordionItem dataTestId="details">
+            <AccordionSummary classNameSummary="test-summary" dataTestId="summary">
+                Test
+            </AccordionSummary>
+        </AccordionItem>,
+    );
+
+    expect(getByTestId('details').getAttribute('open')).toBeNull();
+
+    fireEvent(getByTestId('summary'), new MouseEvent('click'));
+
+    expect(container).toBeInTheDocument();
+    expect(getByTestId('details').getAttribute('open')).not.toBeNull();
 });
